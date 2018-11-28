e550d0473a1f021aed17970ab7cac8a23096bac6
elizzzabethrae <elizabethwarners@gmail.com>
2018-11-28 08:34:03 -0500

    checkpoint 5 completed

---
+++ main.js
@@ -0,0 +1,16 @@
+const greetings = require("./greeting.js");
+const math = require("./math.js");
+
+// "Hello"
+greetings.greetingEnglish();
+
+// "Konichiwa"
+greetings.greetingJapanese();
+
+greetings.greetingSpanish();
+
+greetings.greetingIcelandic();
+
+math.twoPlustwo();
+
+math.tenMinusfive();
---
+++ math.js
@@ -0,0 +1,7 @@
+exports.twoPlustwo = function() {
+    console.log(2+2);
+}
+
+exports.tenMinusfive = function() {
+    console.log(10-5);
+}
